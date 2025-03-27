adc77c6bdd21836c98f18a94e35fdd9915f67b04
kermage <genealyson.torcende@gmail.com>
2025-03-27 22:37:00 +0800

    handle filtered value

--- src/MetaHelpers.php
+++ src/MetaHelpers.php
@@ -13,13 +13,13 @@ class MetaHelpers {
 
 	public static function default( $value, int $object_id, string $meta_key, bool $single, string $meta_type ) {
 
-		$schema = apply_filters( "themeplate_{$meta_type}_meta_{$meta_key}_schema", array() );
+		$schema = (array) apply_filters( "themeplate_{$meta_type}_meta_{$meta_key}_schema", array() );
 
-		if ( empty( $schema[ $meta_key ] ) ) {
+		if ( empty( $schema[ $meta_key ] ) || ! is_array( $schema[ $meta_key ] ) ) {
 			return $value;
 		}
 
-		return $schema[ $meta_key ]['default'];
+		return $schema[ $meta_key ]['default'] ?? $value;
 
 	}
 
---
+++ tests/MetaHelpersTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @package ThemePlate
+ */
+
+namespace Tests;
+
+use ThemePlate\Meta\MetaHelpers;
+use WP_UnitTestCase;
+
+class MetaHelpersTest extends WP_UnitTestCase {
+	public static function for_default_values(): array {
+		return array(
+			'empty'      => array( '', '' ),
+			'missing'    => array(
+				array( 'test' => array( 'default' => '' ) ),
+				'',
+			),
+			'no_default' => array(
+				array( 'key' => array( 'test' => 'value' ) ),
+				'',
+			),
+			'custom'     => array(
+				array( 'key' => array( 'default' => 'value' ) ),
+				'value',
+			),
+			'invalid'    => array(
+				array( 'key' => (object) array( 'default' => 'value' ) ),
+				'',
+			),
+		);
+	}
+
+	/** @dataProvider for_default_values */
+	public function test_default_values( $schema, string $expected ): void {
+		$meta_type = 'post';
+		$meta_key  = 'key';
+
+		add_filter(
+			'themeplate_' . $meta_type . '_meta_' . $meta_key . '_schema',
+			function () use ( $schema ) {
+				return $schema;
+			}
+		);
+
+		$this->assertSame( $expected, MetaHelpers::default( '', 1, $meta_key, true, $meta_type ) );
+	}
+}
